fd1fbf03740755cdac33be9f161b0ca6741d8a94
KimDongWoo <dw81770@gmail.com>
2024-04-08 20:47:48 +0900

    java.algo.4

--- week_4/boj_11651.java
+++ week_4/boj_11651.java
@@ -1,2 +1,35 @@
-package week_4;public class boj_11651 {
+package week_4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class boj_11651 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] list = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                list[i][j] = (sc.nextInt());
+            }
+        }
+
+
+        Arrays.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return Integer.compare(o1[0], o2[0]);
+                }
+                return Integer.compare(o1[1], o2[1]);
+            }
+        });
+
+        // 정렬된 좌표 출력
+        for (int i = 0; i < n; i++) {
+            System.out.println(list[i][0] + " " + list[i][1]);
+        }
+    }
 }
